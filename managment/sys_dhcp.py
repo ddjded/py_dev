aea7adb0818f38101fb726049b2b2b0e6666f9b6
Yuriy Kolosovskyi <y.kolosovskyi@steripackgroup.com>
2018-03-11 21:06:51 +0100

    add

--- managment/sys_dhcp.py
+++ managment/sys_dhcp.py
@@ -1,13 +1,19 @@
 import subprocess
 
+SERVER = input("DNS Server - ")
 SCOPE = input("Scope DNS - ")
 IP = input("IP - ")
 MAC = input("MAC - ")
 DESCRIPTION = input("Description - ")
 
-netshcmd = subprocess.Popen('netsh ', shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE )
-output, errors =  netshcmd.communicate()
+netshcmd = subprocess.Popen(
+    'netsh dhcp server ' + SERVER + ' scope ' 
+    + SCOPE + ' add reservedip ' 
+    + IP + ' ' 
+    + MAC + ' ' 
+    + DESCRIPTION + ' "" BOTH', shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+output, errors = netshcmd.communicate()
 if errors:
-    print("WARNING: woow", errors)
+    print("WARNING: ", errors)
 else:
-    print("SUCCESS: ok ", output)
+    print("SUCCESS: ", output)
